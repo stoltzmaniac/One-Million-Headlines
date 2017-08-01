506c1a0dd01ef0ea1dad71c030958fd4c6104665
Scott Stoltzman <scott@stoltzmaniac.com>
2017-07-31 18:25:48 -0600

    tidy text data frame created

--- sandbox.R
+++ sandbox.R
@@ -3,8 +3,16 @@
 # Data is from Kaggle https://www.kaggle.com/therohk/million-headlines
 
 library(data.table)
+library(tidyverse)
+library(ggplot2)
+library(tidytext)
 
 raw.data = fread('abcnews-date-text.csv')
 head(raw.data)
 df = raw.data
 df$publish_date = as.Date(as.character(df$publish_date),'%Y%m%d')
+
+text_df = df %>%
+  unnest_tokens(word, headline_text)
+
+nrow(text_df) # 6902242 ~ roughly 6.9 Million words!
